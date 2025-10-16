6ab8e58fca250de4b446c63d5aab3f1a9b3498e3
Faiqueali1 <faiquealikhan1@gmail.com>
2025-10-16 18:10:17 +0500

    Metrics endpoint

--- src/server.js
+++ src/server.js
@@ -3,15 +3,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import hrRoutes from "./routes/hr.routes.js";
+import client from "prom-client";
 
 dotenv.config();
 const app = express();
-
+// Create a registry
+const register = new client.Registry();
 app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 
 // HR routes
 app.use("/employees", hrRoutes);
+
+// Metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get("/", (req, res) => res.json({message: "HR Service Running 🏢"}));
 
